ea39d561031be6d4e918b53fa74d2d48c67f9e87
vagrant <vagrant@vagrant.vm>
2019-12-15 02:26:36 +0000

    small design edits

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 from flask import Flask, render_template, url_for, request, redirect, flash, jsonify
-from flask import session as login_session
-import random, string
-
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
